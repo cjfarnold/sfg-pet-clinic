8477cfdcd7c831b38e62f1fd3352429f5a721f38
cjfarnold <cjfarnold@gmail.com>
2020-07-15 21:49:36 +0530

    first push

---
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
